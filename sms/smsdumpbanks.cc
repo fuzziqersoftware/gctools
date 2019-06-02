6614892fb5403ed9179a32fe0934ac69a6f32d83
Martin Michelsen <mjem@wildblue.net>
2019-06-02 14:49:25 -0700

    add soundfont-like output for smsdumpbanks

--- sms/smsdumpbanks.cc
+++ sms/smsdumpbanks.cc
@@ -31,6 +31,14 @@ string name_for_note(uint8_t note) {
 
 
 
+string base_filename_for_sound(const Sound& s) {
+  return string_printf("sample-%s-%" PRIX64 "-%08" PRIX32 "-%08" PRIX32
+      "-%08" PRIX32, s.source_filename.c_str(), s.source_offset,
+      s.sound_id, s.aw_file_index, s.wave_table_index);
+}
+
+
+
 int main(int argc, char** argv) {
   if (argc != 3) {
     fprintf(stderr, "usage: %s bank_directory output_directory\n", argv[0]);
@@ -39,6 +47,7 @@ int main(int argc, char** argv) {
 
   auto env = load_sound_environment(argv[1]);
 
+  // generate text file
   for (const auto& ibank_it : env.instrument_banks) {
     const auto& ibank = ibank_it.second;
 
@@ -72,6 +81,92 @@ int main(int argc, char** argv) {
     }
   }
 
+  // generate soundfont text file
+  {
+    string filename = string_printf("%s/metadata-sf.txt", argv[2]);
+    auto f = fopen_unique(filename.c_str(), "wt");
+
+    fprintf(f.get(), "[Samples]\n\n");
+    for (const auto& bank_it : env.sample_banks) {
+      for (const Sound& s : bank_it.second) {
+        string sound_basename = base_filename_for_sound(s);
+        fprintf(f.get(), "\
+    SampleName=%s\n\
+        SampleRate=%zu\n\
+        Key=%hhu\n\
+        FineTune=0\n\
+        Type=1\n\n", sound_basename.c_str(), s.sample_rate, s.base_note);
+      }
+    }
+
+    fprintf(f.get(), "[Instruments]\n\n");
+    for (const auto& ibank_it : env.instrument_banks) {
+      const auto& ibank = ibank_it.second;
+      for (const auto& inst_it : ibank.id_to_instrument) {
+        string instrument_name = string_printf("inst_%08" PRIX32 "_%08" PRIX32, ibank.id, inst_it.first);
+        fprintf(f.get(), "    InstrumentName=%s\n\n", instrument_name.c_str());
+        for (const auto& key_region : inst_it.second.key_regions) {
+          for (const auto& vel_region : key_region.vel_regions) {
+            if (!vel_region.sound) {
+              fprintf(stderr, "warning: sound missing for instrument=%08" PRIX32 ":%08" PRIX32 " key=[%hhu,%hhu] vel=[%hhd,%hhd]: sound id 0x%hX, frequency multiplier %g, base note %02hhX\n",
+                  ibank.id, inst_it.first, key_region.key_low, key_region.key_high,
+                  vel_region.vel_low, vel_region.vel_high, vel_region.sound_id,
+                  vel_region.freq_mult, vel_region.base_note);
+            } else {
+              string basename = base_filename_for_sound(*vel_region.sound);
+              uint8_t base_note = vel_region.base_note ? vel_region.base_note : vel_region.sound->base_note;
+              fprintf(f.get(), "\
+        Sample=%s\n\
+            Z_LowKey=%hhu\n\
+            Z_HighKey=%hhu\n\
+            Z_LowVelocity=%hhu\n\
+            Z_HighVelocity=%hhu\n\
+            Z_sampleModes=1\n\
+            Z_overridingRootKey=%hhu\n\
+            Z_Modulator=(NoteOnVelocity,ReverseDirection,Unipolar,Linear), initialFilterFc, 0, (NoteOnVelocity,ReverseDirection,Unipolar,Switch), 0\n\n",
+                  basename.c_str(), key_region.key_low, key_region.key_high,
+                  vel_region.vel_low, vel_region.vel_high, base_note);
+            }
+          }
+        }
+      }
+    }
+
+    fprintf(f.get(), "[Presets]\n\n");
+    for (const auto& ibank_it : env.instrument_banks) {
+      const auto& ibank = ibank_it.second;
+      for (const auto& inst_it : ibank.id_to_instrument) {
+        string instrument_name = string_printf("inst_%08" PRIX32 "_%08" PRIX32, ibank.id, inst_it.first);
+        fprintf(f.get(), "\
+    PresetName=preset_%s\n\
+        Bank=%" PRIu32 "\n\
+        Program=%" PRIu32 "\n\
+\n\
+        Instrument=%s\n\
+            L_LowKey=0\n\
+            L_HighKey=127\n\
+            L_LowVelocity=0\n\
+            L_HighVelocity=127\n\n",
+            instrument_name.c_str(), ibank.id, inst_it.first, instrument_name.c_str());
+      }
+    }
+
+    fprintf(f.get(), "\
+[Info]\n\
+Version=2.1\n\
+Engine=\n\
+Name=\n\
+ROMName=\n\
+ROMVersion=\n\
+Date=\n\
+Designer=\n\
+Product=\n\
+Copyright=\n\
+Editor=\n\
+Comments=\n");
+  }
+
+  // export samples
   for (const auto& wsys_it : env.sample_banks) {
     for (const auto& s : wsys_it.second) {
       auto samples = s.samples();
@@ -80,14 +175,13 @@ int main(int argc, char** argv) {
             s.source_filename.c_str(), s.source_offset, s.source_size);
         continue;
       }
-      string filename = string_printf("%s/sample-%s-%" PRIX64 "-%08" PRIX32
-          "-%08" PRIX32 "-%08" PRIX32 ".wav", argv[2],
-          s.source_filename.c_str(), s.source_offset, s.sound_id,
-          s.aw_file_index, s.wave_table_index);
+      string basename = base_filename_for_sound(s);
+      string filename = string_printf("%s/%s", argv[2], basename.c_str());
       save_wav(filename.c_str(), samples, s.sample_rate, s.num_channels);
     }
   }
 
+  // export sequences
   for (const auto& s : env.sequence_programs) {
     string fn = string_printf("%s/sequence-%" PRIu32 "-%s.bms", argv[2],
         s.second.index, s.first.c_str());
