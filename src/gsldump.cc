36ca306d7bc5fbe066168ce674d91c34df35e095
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2022-08-03 10:56:10 -0700

    support PSOBB GSL files

--- src/gsldump.cc
+++ src/gsldump.cc
@@ -13,24 +13,23 @@
 
 using namespace std;
 
+template <typename U32T>
 struct GSLEntry {
   char name[0x20];
   be_int32_t unknown;
-  be_uint32_t size;
+  U32T size;
   be_int32_t unknown2[2];
 } __attribute__((packed));
 
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-    return -1;
-  }
+struct GSLEntryBE : GSLEntry<be_uint32_t> { };
+struct GSLEntryLE : GSLEntry<le_uint32_t> { };
 
-  const char* input_filename = argv[1];
-  auto f = fopen_unique(input_filename, "rb");
+template <typename EntryT, size_t EntryCount>
+void extract_gsl_t(const char* filename) {
+  auto f = fopen_unique(filename, "rb");
 
-  array<GSLEntry, 0x100> entries;
-  freadx(f.get(), entries.data(), sizeof(GSLEntry) * entries.size());
+  array<EntryT, EntryCount> entries;
+  freadx(f.get(), entries.data(), sizeof(EntryT) * entries.size());
 
   for (const auto& entry : entries) {
     if (!entry.name[0]) {
@@ -44,8 +43,32 @@ int main(int argc, char* argv[]) {
     fseek(f.get(), (ftell(f.get()) + 0x7FF) & (~0x7FF), SEEK_SET);
 
     save_file(
-        string_printf("%s-%s", input_filename, entry.name),
+        string_printf("%s-%s", filename, entry.name),
         freadx(f.get(), size));
+  }
+}
+
+int main(int argc, char* argv[]) {
+  const char* input_filename = nullptr;
+  bool bb_format = false;
+  for (int x = 1; x < argc; x++) {
+    if (!strcmp(argv[x], "--bb")) {
+      bb_format = true;
+    } else if (!input_filename) {
+      input_filename = argv[x];
+    } else {
+      throw invalid_argument("excess option: " + string(argv[x]));
+    }
+  }
+  if (!input_filename) {
+    fputs("Usage: gsldump [--bb] <filename>\n", stderr);
+    return 1;
+  }
+
+  if (bb_format) {
+    extract_gsl_t<GSLEntryLE, 0x800>(input_filename);
+  } else {
+    extract_gsl_t<GSLEntryBE, 0x100>(input_filename);
   }
 
   return 0;
